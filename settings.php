192682b55ec370c9d888ffe7a298ba963962e88d
be4r <cid33@mail.ru>
2021-11-18 00:41:11 +0300

    #

--- settings.php
+++ settings.php
@@ -6,22 +6,20 @@
 
 // Streak graph picture
 $commits = [
-	/* columns
-	'      10>|      20>|      30>|      40>|      50>|  ' < exactly 52 characters */
-	'   *####     #      ###   #####   #   #    *###*    ',  // Sun
-	'   #                  #   #       #  #    #     #   ',  // Mon
-	'   #       #####      #   #       # #     #     #   ',  // Tue
-	'   *###*     #        #   ###     ##*     #     #   ',  // Wed
-	'       #     #        #   #       #  #    #     #   ',  // Thu
-	'       #     #    #   #   #       #   #   #     #   ',  // Fri
-	'   ####*   #####   *##*   #####   #    #   *###*    ',  // Sat
+	'                                                    ',
+	'  ### ### ##  ###  ### ### ### ### ###   ## #    #  ',
+	'  #   #   # #   #  #    #  # # # # # #  # # #    #  ',
+	'  #   ### ##  ###  #    #  # # # # # #  # # ###  #  ',
+	'  #   #   # # # #  #    #  # # # # # #  # # # #     ',
+	'  ### ### ##  #### ###  #  ### # # ### ## # ###  #  ',
+	'                                                    ',
 ];
 //  *  for 10 commmits in that day (lighter color)
 //  #  for 20 commmits in that day (darker color)
 
 
 // Repository origin
-$origin = 'https://github.com/maximal/gerda.git';
+$origin = 'https://github.com/be4r/_.git';
 
 
 // Output shell file
